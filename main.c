4e456d66e4d027f1bb7bc2cd411f30cf62044f94
MWHunter <s0521458@student.rockvalleycollege.edu>
2023-02-14 23:38:34 -0600

    more comments and fix bug

--- main.c
+++ main.c
@@ -22,24 +22,28 @@ int main(int argc, char *argv[]) {
     char input[MAX_INPUT];
     char *commands[MAX_COMMANDS];
 
-    // Check arguments
+    // Check if we were provided multiple batch files, which isn't allowed
     if (argc > 2) {
         write(STDERR_FILENO, error_message, strlen(error_message));
         exit(1);
     }
 
-    // Open batch file if specified
+    // Check if we are running in batch mode
     if (argc == 2) {
         batch_file = fopen(argv[1], "r");
         if (batch_file == NULL) {
+            // Not a valid file
             write(STDERR_FILENO, error_message, strlen(error_message));
             exit(1);
         }
     }
 
+    // Flag to stop running, due to the need to break out of inner loops
     int isRunning = 1;
+    // infinite loop to keep asking the user questions
     while (isRunning) {
         if (batch_file) {
+            // we get our input from a file
             if (fgets(input, MAX_INPUT, batch_file) == NULL) {
                 return 0;  // End of batch file
             }
@@ -47,6 +51,7 @@ int main(int argc, char *argv[]) {
         } else {
             // Only print the prompt when not executing a batch file
             printf("smash> ");
+            // Flush because we don't use a newline at the end, which stops some automatic behavior
             fflush(stdout);
 
             if (fgets(input, MAX_INPUT, stdin) == NULL) {
@@ -59,6 +64,7 @@ int main(int argc, char *argv[]) {
 
         // Parse multiple commands separated by ';' or '&'
         char *separator = ";&";
+
         char *command = strtok(input, separator);
         int i = 0;
         while (command != NULL) {
@@ -71,13 +77,14 @@ int main(int argc, char *argv[]) {
         while (commands[j] != NULL) {
             char *output_str = NULL;
 
-            // Check for input/output redirection
+            // Check for output redirection
             char *output_redirect = strchr(commands[j], '>');
             if (output_redirect != NULL) {
                 output_str = strtok(output_redirect + 1, " ");
                 commands[j][output_redirect - commands[j]] = '\0';
             }
 
+            // Open the file for redirection while we can still exit early
             FILE *output_file = NULL;
             if (output_str != NULL) {
                 output_file = fopen(output_str, "w");
@@ -96,18 +103,19 @@ int main(int argc, char *argv[]) {
             while (*end == ' ' && end > command) {
                 end--;
             }
+            // Make the string end where there is whitespace
             *(end + 1) = '\0';
 
-            if (strcmp(input, "exit") == 0) {
+            if (strcmp(command, "exit") == 0) {
                 // If only all commands were this simple
                 isRunning = 0;
                 break;
-            } else if (strncmp(input, "cd ", 3) == 0) {
+            } else if (strncmp(command, "cd ", 3) == 0) {
                 // cd command
                 if (chdir(input + 3) == -1) {
                     write(STDERR_FILENO, error_message, strlen(error_message));
                 }
-            } else if (strncmp(input, "path", 4) == 0) {
+            } else if (strncmp(command, "path", 4) == 0) {
                 // path command
                 char *op = strtok(input + 4, " ");
                 char *arg = strtok(NULL, " ");
@@ -119,7 +127,7 @@ int main(int argc, char *argv[]) {
                 update_paths(paths, op, arg);
             } else {
                 // Execute command
-                execute_command(commands[j], paths, output_file);
+                execute_command(command, paths, output_file);
             }
 
             j++;
@@ -198,7 +206,7 @@ void update_paths(char *paths[], char *op, char *arg) {
             if (paths[i] != NULL && strcmp(paths[i], arg) == 0) {
                 found = 1;
             }
-            if (found && i != MAX_PATHS) {
+            if (found && i != MAX_PATHS - 1) {
                 paths[i] = paths[i + 1];
             }
         }
